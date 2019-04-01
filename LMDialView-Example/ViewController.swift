8cc1fb2a7178bcf36b2961541a5ab80d090f97b8
ZhouYingguo <ingocraft118@gmail.com>
2019-04-01 15:40:11 +0800

    tidy.

--- LMDialView-Example/ViewController.swift
+++ LMDialView-Example/ViewController.swift
@@ -11,31 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     
     private var randomColors = [UIColor]()
-
     private var dialView: LMDialView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dialView = LMDialView()
-        dialView.dataSource = self
-        dialView.delegate = self
-        view.addSubview(dialView)
-        
-        dialView.translatesAutoresizingMaskIntoConstraints = false
-        let constraints = [
-            dialView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            dialView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            dialView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            dialView.heightAnchor.constraint(equalToConstant: 48),
-            ]
-        NSLayoutConstraint.activate(constraints)
-        
-        for _ in 0...48 {
-            let red = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
-            let green = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
-            let blue = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
-            randomColors.append(UIColor.init(red: red, green: green, blue: blue, alpha: 1.0))
-        }
+        generateRandomData()
+        setupSubviews()
     }
 
 }
@@ -54,7 +35,6 @@ extension ViewController: SPIDialViewDataSource {
     func dialView(_ dialView: LMDialView, scaleAt index: Int) -> LMDialViewCell {
         let cell = dialView.dequeueReusableCell(for: index)
         
-
         let isStartCell = index == 0
         if isStartCell {
             cell.backgroundColor = UIColor.black
@@ -63,5 +43,36 @@ extension ViewController: SPIDialViewDataSource {
         }
         
         return cell
+    }
+}
+
+// MARK: private
+private extension ViewController {
+    func generateRandomData() {
+        for _ in 0...48 {
+            let red = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+            let green = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+            let blue = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+            randomColors.append(UIColor.init(red: red, green: green, blue: blue, alpha: 1.0))
+        }
+    }
+}
+
+// MARK: UI
+private extension ViewController {
+    func setupSubviews() {
+        dialView = LMDialView()
+        dialView.dataSource = self
+        dialView.delegate = self
+        view.addSubview(dialView)
+        
+        dialView.translatesAutoresizingMaskIntoConstraints = false
+        let constraints = [
+            dialView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            dialView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            dialView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            dialView.heightAnchor.constraint(equalToConstant: 48),
+            ]
+        NSLayoutConstraint.activate(constraints)
     }
 }
