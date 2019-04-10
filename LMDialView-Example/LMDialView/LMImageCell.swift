86aff3dd7471ff685da9d966a53e0927bf6a7068
ZhouYingguo <ingocraft118@gmail.com>
2019-04-10 16:05:50 +0800

    Add resources.

---
+++ LMDialView-Example/LMDialView/LMImageCell.swift
@@ -0,0 +1,33 @@
+//
+//  LMImageCell.swift
+//  LMDialView-Example
+//
+//  Created by Liam on 2019/4/10.
+//  Copyright © 2019 Liam. All rights reserved.
+//
+
+import UIKit
+
+class LMImageCell: UIView {
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        setupSubviews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+// MARK: UI
+private extension LMImageCell {
+    func setupSubviews() {
+        // init
+        
+        // add
+        
+        // layout
+    }
+}
